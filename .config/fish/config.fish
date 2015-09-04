773a6a996efe75cdee28cb95f21421021470f3ee
Viktor Kronvall <viktor.kronvall@gmail.com>
2015-09-04 19:04:44 +0200

    Remove fish config from repo.

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-set -x EDITOR nvim
-eval sh $HOME/.config/base16-shell/base16-ocean.dark.sh
-set -gx GOPATH $HOME/go
-set -gx PATH $PATH $GOPATH/bin
-set -gx CUBBY $HOME/cubby
-
-set -x DOCKER_HOST tcp://192.168.59.103:2376
-set -x DOCKER_CERT_PATH /Users/considerate/.boot2docker/certs/boot2docker-vm
-set -x DOCKER_TLS_VERIFY 1
